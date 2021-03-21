5717ad759b30635d9aed42e9c4966a39df54c510
conor <conorjm89@gmail.com>
2021-03-21 20:13:41 +0000

    Updates

---
+++ static/js/main.425d668d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpshopping-cart"]=this["webpackJsonpshopping-cart"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var c=n(0),r=n.n(c),s=n(3),a=n.n(s),i=(n(8),n(1));var o=function(){return Object(i.jsx)("div",{className:"App",children:"Ready to React!"})};a.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},8:function(t,e,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.425d668d.chunk.js.map
