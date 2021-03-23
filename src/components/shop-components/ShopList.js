0061c48f27198f081788da678160d9c1ebd7be29
conor <conorjm89@gmail.com>
2021-03-23 23:25:07 +0000

    Add shop items

--- src/components/shop-components/ShopList.js
+++ src/components/shop-components/ShopList.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import shopItems from './shopItems.json';
+import Guinness from '../../img/guinness.jpg';
+import Whiskey from '../../img/cocktail.jpeg';
+import Lager from '../../img/lager.jpeg';
+import Cocktail from '../../img/cocktail.jpeg';
+import Coffee from '../../img/coffee.jpeg';
+import Water from '../../img/water.jpeg';
+import Milk from '../../img/milk.jpeg';
+
 
 function ShopList(props) {
+  const imgArray = [
+    Guinness,
+    Whiskey,
+    Lager,
+    Cocktail,
+    Coffee,
+    Water,
+    Milk
+  ];
+
   return (
     <div className="ShopList">
-      Display the list of drinks to buy
+      {shopItems.map((item, index) => (
+        <div key={item.id}>
+          <h3>{item.name}</h3>
+          <img src={imgArray[index]} alt={item.name}/>
+          <p>{item.price}</p>
+          <button>Buy</button>
+        </div>
+      ))}
     </div>
   );
 }
